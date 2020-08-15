0174c2c5b7cefa3968bb5efe23d017722fd3c6d2
Thanh Nguyen <thanh.h.nguyen23@gmail.com>
2020-08-14 20:28:42 -0400

    add unique title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-tour-of-heroes';
+  // title = 'Tour of Heroes';
+  title = 'hi Thanh, you are using angular 9\nTour of Heroes!';
 }
