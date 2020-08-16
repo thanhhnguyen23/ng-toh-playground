0862872b6588b7f4b45e3205b98b041355a6ac5d
Thanh Nguyen <thanh.h.nguyen23@gmail.com>
2020-08-15 18:28:41 -0700

    import mock hero data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HEROES } from './mock-heroes';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,5 @@ import { Component } from '@angular/core';
 export class AppComponent {
   // title = 'Tour of Heroes';
   title = 'hi Thanh, you are using angular 9\nTour of Heroes!';
+  heroes = HEROES;
 }
