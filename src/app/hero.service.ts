f7952ddd11e8f2578552074cb14407fc0107b145
Thanh Nguyen <thanh.h.nguyen23@gmail.com>
2020-09-20 15:54:38 -0400

    get hero(es); getOne & getAll heroes

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -5,14 +5,21 @@ import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class HeroService {
-  getHeroes(): Observable<Hero[]>{
-    // TODO -- send message _after_ fetching the heroes 
-    this.messageService.add('HeroService: fetched heroes');
+  getHeroes(): Observable<Hero[]> {
+    // TODO -- send message _after_ fetching the heroes
+
+    this.messageService.add("HeroService: fetched heroes");
     return of(HEROES);
   }
 
-  constructor(private messageService: MessageService) { }
+  getHero(id: number): Observable<Hero> {
+    // TODO: send the message _after_ fetching the hero
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find((hero) => hero.id === id));
+  }
+
+  constructor(private messageService: MessageService) {}
 }
