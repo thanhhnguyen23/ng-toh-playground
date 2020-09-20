932a6ab421545ffdabca1bbcd2e335a81d3e93fb
Thanh Nguyen <thanh.h.nguyen23@gmail.com>
2020-09-20 14:25:08 -0400

    revise title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,6 @@ import { HEROES } from './mock-heroes';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  // title = 'Tour of Heroes';
-  title = 'hi Thanh, you are using angular 9\nTour of Heroes!';
+  title = 'Tour of Heroes';
   heroes = HEROES;
 }
