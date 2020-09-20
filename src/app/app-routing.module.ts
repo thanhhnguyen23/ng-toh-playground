ed1577a6bf3383015095b82d4f8557ba919aed21
Thanh Nguyen <thanh.h.nguyen23@gmail.com>
2020-09-20 14:39:35 -0400

    add Routes; add router-outlet selector from @angular/routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { HeroesComponent } from './heroes/heroes.component';
 
 const routes: Routes = [
